f55bf2e4ff6ba61da4e37a86e353a2bec8661f21
Archer Murray <amurray@cub.uca.edu>
2022-06-02 13:08:33 -0500

    Implemented test for genetic algorithm

--- pyl/src/ml/Main.java
+++ pyl/src/ml/Main.java
@@ -9,13 +9,19 @@ public final class Main
 {
 	public static void main(String[] args)
 	{
-		GeneticLearner gl = new GeneticLearner();
+		GeneticLearner gl = new GeneticLearner(1500);
 		double eval = gl.evaluate(true);
-		System.out.println("Generation 0 evaluation: " + eval);
-		for (int i = 1; i <= 10; i++) {
-			gl.playGeneration(true);
+		System.out.println("0 min. evaluation: " + eval);
+		// Run for 8 hours
+		for (int i = 1; i <= 480; i++) {
+			long t1 = System.currentTimeMillis();
+			long t2 = t1;
+			while (t2 - t1 < 60000) {
+				gl.playGeneration(true);
+				t2 = System.currentTimeMillis();
+			}
 			eval = gl.evaluate(true);
-			System.out.println("Generation " + i + " evaluation: " + eval);
+			System.out.println(i + " min. evaluation: " + eval);
 		}
 	}
 }
